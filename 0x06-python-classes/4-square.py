c4eadd6b335a35bf17475245974fd2c89987ce07
helykish <helykish@gmail.com>
2024-01-23 13:51:31 -0800

    task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+# 0-square.py alx task
+"""A module that defines a square """
+
+
+class Square:
+    """A class tt representing square"""
+
+    def __init__(self, size=0):
+        """Initial square class
+        Args:
+            size: represnets the size of the square defined
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
+        """
+
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieves area of square"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """
+        Calculate area of the square
+        Returns: The area of the square
+        """
+
+        return (self.__size ** 2)
