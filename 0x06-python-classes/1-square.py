9b2a3cd37b7e13d267d5de9f83c4d35331168c81
helykish <helykish@gmail.com>
2024-01-23 10:41:50 -0800

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 0-square.py alx project
+"""Entry point: Defines a square """
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initiual square class
+        Args: size - represneting size of the square defined
+        """
+
+        self.__size = size
