be1ced6135f35681718d855fda5f19b05f937460
helykish <111018650+helykish@users.noreply.github.com>
2024-04-27 02:35:13 +0300

    5. Response header value #1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Displays the X-Request-Id header variable of a request to a given URL
+"""script show X-Request-Id header var of a request to a available URL
 """
 import sys
 import requests
