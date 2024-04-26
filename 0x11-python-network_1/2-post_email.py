8cfe82f84894abd5d63b39c9b0c877cec442e233
helykish <111018650+helykish@users.noreply.github.com>
2024-04-27 02:25:39 +0300

    2. POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL
+"""write a script that:
+- check in a URL
 - sends a POST request to the passed URL
-- takes email as a parameter
-- displays the body of the response
+- check email as a parameter
+- show the body of the response
 """
 import sys
 import urllib.parse
